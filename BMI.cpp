f6185c8eefb3378e5c46e84a24929f4cd293b7c1
canzonadzka <canzonadzka@gmail.com>
2026-03-09 21:23:23 +0800

    membuat prosedur inputData untuk memasukkan berat dan tinggi

--- BMI.cpp
+++ BMI.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void inputData(double &berat, double &tinggi){
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
+
 int main() {
 
     double berat;
     double tinggi;
 
     cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
+
+    inputData(berat, tinggi);
 
     return 0;
 }
